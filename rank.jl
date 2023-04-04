b7f22fbe63cd84e3d904a7fe33e9e7e3e7b3a070
KincaidY <ky2rh@virginia.edu>
2023-04-03 22:41:13 -0400

    Specify prep_locations col types

--- rank.jl
+++ rank.jl
@@ -17,7 +17,7 @@ DATA_DIR = "../"
 
 walk = CSV.read(joinpath(DATA_DIR, "walkscore.txt"), delim="\t")
 precip = CSV.read(joinpath(DATA_DIR, "nrmpcp.txt"), delim="    ")
-precip_locations = CSV.File(IOBuffer(join(precip["NORMALS 1981-2010"], "\n")), header=["zip", "city", "state"])
+precip_locations = CSV.File(IOBuffer(join(precip["NORMALS 1981-2010"], "\n")), header=["zip", "city", "state"], types=[String, String, String])
 precip_locations = DataFrames.DataFrame(precip_locations)
 precip_data = CSV.File(IOBuffer(join(precip["                 YRS  JAN   FEB   MAR   APR   MAY   JUN   JUL   AUG   SEP   OCT   NOV   DEC   ANN"], "\n")), header=["YRS", "JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC", "ANN"])
 precip_data = DataFrames.DataFrame(precip_data)
