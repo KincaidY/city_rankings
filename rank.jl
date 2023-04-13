4847e77915af62fd77734722667cf09d893b1dda
KincaidY <ky2rh@virginia.edu>
2023-04-12 20:47:02 -0400

    Write climate norms data reading function

--- rank.jl
+++ rank.jl
@@ -15,10 +15,20 @@ weather below 30 or above 100
 """
 DATA_DIR = "../"
 
+function read_climate_norms(path)
+    data = CSV.read(path, delim="    ")
+    locations = CSV.File(IOBuffer(join(data["NORMALS 1981-2010"], "\n")), header=["zip", "city", "state"], types=[String, String, String])
+    locations = DataFrames.DataFrame(data)
+    data = CSV.File(IOBuffer(join(data["                 YRS  JAN   FEB   MAR   APR   MAY   JUN   JUL   AUG   SEP   OCT   NOV   DEC   ANN"], "\n")), header=["YRS", "JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC", "ANN"])
+    data = DataFrames.DataFrame(data)
+    data = hcat(precip_locations, data)
+    data[["zip", "city", "state", "YRS"]] = strip.(data[["zip", "city", "state", "YRS"]])
+    data
+end
+
+# function read_cloudy_data(path)
+
 walk = CSV.read(joinpath(DATA_DIR, "walkscore.txt"), delim="\t")
-precip = CSV.read(joinpath(DATA_DIR, "nrmpcp.txt"), delim="    ")
-precip_locations = CSV.File(IOBuffer(join(precip["NORMALS 1981-2010"], "\n")), header=["zip", "city", "state"], types=[String, String, String])
-precip_locations = DataFrames.DataFrame(precip_locations)
-precip_data = CSV.File(IOBuffer(join(precip["                 YRS  JAN   FEB   MAR   APR   MAY   JUN   JUL   AUG   SEP   OCT   NOV   DEC   ANN"], "\n")), header=["YRS", "JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC", "ANN"])
-precip_data = DataFrames.DataFrame(precip_data)
-precip = hcat(precip_locations, precip_data)
+precip = read_weather_data(joinpath(DATA_DIR, "nrmpcp.txt"))
+temps = read_weather_data(joinpath(DATA_DIR, "nrmmax.txt"))
+# clouds = read_weather_data(joinpath(DATA_DIR, "clpcdy20.txt"))
